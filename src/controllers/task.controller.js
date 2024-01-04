3356223daddbd76483dcbd3098bf2d1929ddbe0c
Ajay Duraisamy <ajayduraisamy@gmail.com>
2024-01-04 10:30:00 +0530

    feat: add get tasks controllers

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -4,15 +4,37 @@ const Task = require("../models/task.model");
 const createTask = async (req, res) => {
   try {
     const task = await Task.create({
-      text: req.body.text
+      text: req.body.text,
     });
-
     res.status(201).json(task);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
 
+// GET ALL TASKS
+const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find().sort({ createdAt: -1 });
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// GET TASK BY ID
+const getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    res.json(task);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
 module.exports = {
-  createTask
+  createTask,
+  getTasks,
+  getTaskById,
 };
