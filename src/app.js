4c311d6cc7ec0f3ddd258cac700050bcc9b22bfb
Ajay Duraisamy <ajayduraisamy@gmail.com>
2025-12-05 17:58:12 +0530

    fix: enable cors to allow frontend access

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/db");
-
+const cors = require("cors");
 const taskRoutes = require("./routes/task.routes");
-
+app.use(cors());
 const app = express();
 app.use(express.json());
 
